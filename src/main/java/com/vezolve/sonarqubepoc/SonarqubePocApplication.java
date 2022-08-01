f761312f67d951de9be3659fdb7610c5739b6c83
Powsikan <Powsikan@vezolve.com>
2022-08-01 12:42:11 +0530

    logs added

--- src/main/java/com/vezolve/sonarqubepoc/SonarqubePocApplication.java
+++ src/main/java/com/vezolve/sonarqubepoc/SonarqubePocApplication.java
@@ -1,12 +1,15 @@
 package com.vezolve.sonarqubepoc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SonarqubePocApplication {
 
     public static void main(String[] args) {
+        log.info("main called");
         SpringApplication.run(SonarqubePocApplication.class, args);
     }
 
--- src/main/java/com/vezolve/sonarqubepoc/controller/TestController.java
+++ src/main/java/com/vezolve/sonarqubepoc/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.vezolve.sonarqubepoc.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 @AllArgsConstructor
+@Slf4j
 public class TestController {
 
     @GetMapping()
     public String greeting() {
+        log.info("greeting called");
         return "Hello User!";
     }
 }
