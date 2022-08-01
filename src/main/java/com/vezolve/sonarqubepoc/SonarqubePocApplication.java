f94ec9910573dcc15af93a17ceb9a2788de0708c
Powsikan <Powsikan@vezolve.com>
2022-08-01 12:28:20 +0530

    initial setup

---
+++ src/main/java/com/vezolve/sonarqubepoc/SonarqubePocApplication.java
@@ -0,0 +1,13 @@
+package com.vezolve.sonarqubepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarqubePocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vezolve/sonarqubepoc/SonarqubePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vezolve.sonarqubepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubePocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
