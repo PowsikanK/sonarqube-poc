450e5c765f785af577a1725d3ba83422bc33a696
Powsikan <Powsikan@vezolve.com>
2022-08-03 15:27:16 +0530

    unit testing fix

--- src/test/java/com/vezolve/sonarqubepoc/controller/TestControllerTest.java
+++ src/test/java/com/vezolve/sonarqubepoc/controller/TestControllerTest.java
@@ -1,7 +1,10 @@
 package com.vezolve.sonarqubepoc.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -9,31 +12,33 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ContextConfiguration(classes = {TestController.class})
+@ExtendWith(SpringExtension.class)
 class TestControllerTest {
     @Autowired
     private TestController testController;
 
     @Test
     void greeting() throws Exception {
-//        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/test/" );
-//        MockMvcBuilders.standaloneSetup(testController)
-//                .build()
-//                .perform(requestBuilder)
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-//                .andExpect(MockMvcResultMatchers.content()
-//                        .string("Hello User!"));
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/test/" );
+        MockMvcBuilders.standaloneSetup(testController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("Hello User!"));
     }
 
     @Test
     void testing() throws Exception {
-//        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/test/123" );
-//        MockMvcBuilders.standaloneSetup(testController)
-//                .build()
-//                .perform(requestBuilder)
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-//                .andExpect(MockMvcResultMatchers.content()
-//                        .string("Hello Tester!"));
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/test/123" );
+        MockMvcBuilders.standaloneSetup(testController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("Hello Tester!"));
     }
 }
