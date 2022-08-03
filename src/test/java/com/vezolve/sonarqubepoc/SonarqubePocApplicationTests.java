8a607dfb3bddc12c9257ed3560c15f1a373f0688
Powsikan <Powsikan@vezolve.com>
2022-08-03 16:28:08 +0530

    contex load test

--- src/test/java/com/vezolve/sonarqubepoc/SonarqubePocApplicationTests.java
+++ src/test/java/com/vezolve/sonarqubepoc/SonarqubePocApplicationTests.java
@@ -1,13 +1,20 @@
 package com.vezolve.sonarqubepoc;
 
+import com.vezolve.sonarqubepoc.controller.TestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SonarqubePocApplicationTests {
 
+    @Autowired
+    TestController testController;
+
     @Test
     void contextLoads() {
+        assertThat(testController).isNotNull();
     }
 
 }
