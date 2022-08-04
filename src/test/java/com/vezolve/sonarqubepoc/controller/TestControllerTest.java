2c8dc41c8795bd225ad0f11cd0f7b99ac26881bf
Powsikan <Powsikan@vezolve.com>
2022-08-04 17:05:45 +0530

    unit tests added

--- src/test/java/com/vezolve/sonarqubepoc/controller/TestControllerTest.java
+++ src/test/java/com/vezolve/sonarqubepoc/controller/TestControllerTest.java
@@ -10,35 +10,67 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @ContextConfiguration(classes = {TestController.class})
 @ExtendWith(SpringExtension.class)
 class TestControllerTest {
     @Autowired
     private TestController testController;
 
+    /**
+     * Method under test: {@link TestController#greeting()}
+     */
     @Test
-    void greeting() throws Exception {
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/test/" );
+    void testGreeting() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/test");
         MockMvcBuilders.standaloneSetup(testController)
                 .build()
                 .perform(requestBuilder)
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
-                .andExpect(MockMvcResultMatchers.content()
-                        .string("Hello User!"));
+                .andExpect(MockMvcResultMatchers.content().string("Hello User!"));
     }
 
+    /**
+     * Method under test: {@link TestController#greeting()}
+     */
     @Test
-    void testing() throws Exception {
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/test/123" );
+    void testGreeting2() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/test", "Uri Variables");
         MockMvcBuilders.standaloneSetup(testController)
                 .build()
                 .perform(requestBuilder)
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
-                .andExpect(MockMvcResultMatchers.content()
-                        .string("Hello Tester!"));
+                .andExpect(MockMvcResultMatchers.content().string("Hello User!"));
+    }
+
+    /**
+     * Method under test: {@link TestController#testing()}
+     */
+    @Test
+    void testTesting() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/test/123");
+        MockMvcBuilders.standaloneSetup(testController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content().string("Hello Tester!"));
+    }
+
+    /**
+     * Method under test: {@link TestController#testing()}
+     */
+    @Test
+    void testTesting2() throws Exception {
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/test/123");
+        getResult.characterEncoding("Encoding");
+        MockMvcBuilders.standaloneSetup(testController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content().string("Hello Tester!"));
     }
 }
+
